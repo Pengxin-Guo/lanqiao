e486c6c547ff0cf5614fc4daf2a6c28452f7ffdd
gpx <1457495424@qq.com>
2019-05-22 11:45:35 +0800

    rm input

--- finals/2017/6.cpp
+++ finals/2017/6.cpp
@@ -5,30 +5,26 @@
 	> Created Time: 2019年05月21日 星期二 20时31分19秒
  ************************************************************************/
 
-// 本地测评正确,提交提示内存超限
+// TLE代码
 #include <iostream>
 using namespace std;
-#define MAX_N 10000
+#define MAX_N 100000
+typedef long long LL;
 
-int n, m, c[MAX_N + 5], a[MAX_N + 5], tmp[MAX_N + 5];
+LL n, m, a[MAX_N + 5], d[MAX_N + 5], tmp[MAX_N + 5];
+// d[n] = d[n] - d[n - 1]
 
-int lowbit(int x) {
-    return x & (-x);
-}
-
-int getsum(int x) {
-    int sum = 0;
-    for (int i = x; i > 0; i -= lowbit(i)) {
-        sum += c[i];
-    }
-    return sum;
-}
-
-void update(int x, int v) {
-    for (int i = x; i <= n; i += lowbit(i)) {
-        c[i] += v;
+void update() {
+    for (int i = 1; i <= n; i++) {
+        d[i] = a[i] - a[i - 1];
     }
     return ;
+}
+
+void getA() {
+    for (int i = 1; i <= n; i++) {
+        a[i] = a[i - 1] + d[i];
+    }
 }
 
 int main() {
@@ -38,32 +34,36 @@ int main() {
     for (int i = 1; i <= n; i++) {
         cin >> v;
         a[i] = v;
-        update(i, v);
     }
+    update();
     for (int i = 0; i < m; i++) {
         int op;
         cin >> op;
         if (op == 1) {
-            int l, r, d;
-            cin >> l >> r >> d;
-            for (int j = l; j <= r; j++) {
-                update(j, d);
-                a[j] += d;
-            }
+            int l, r, x;
+            cin >> l >> r >> x;
+            d[l] += + x;
+            d[r + 1] -= x;
         } else if (op == 2) {
             int l1, r1, l2, r2;
             cin >> l1 >> r1 >> l2 >> r2;
+            getA();
             for (int j = 0; j <= r2 - l2; j++) {
                 tmp[j] = a[l2 + j];
-                update(l1 + j, a[l2 + j] - a[l1 + j]);
             }
             for (int j = 0; j <= r1 - l1; j++) {
                 a[l1 + j] = tmp[j];
             }
+            update();
         } else if (op == 3) {
             int l, r;
             cin >> l >> r;
-            cout << getsum(r) - getsum(l - 1) << endl;
+            getA();
+            LL sum = 0;
+            for (int j = l; j <= r; j++) {
+                sum += a[j];
+            }
+            cout << sum << endl;
         }
     }
     return 0;
