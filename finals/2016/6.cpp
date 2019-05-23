e68f7dc897c6f2e68b91903a34d3e554eb70226f
gpx <1457495424@qq.com>
2019-05-23 09:50:59 +0800

    modify 6.cpp but not finish

--- finals/2016/6.cpp
+++ finals/2016/6.cpp
@@ -6,6 +6,8 @@
  ************************************************************************/
 
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
 #define MAX_N 100000
 #define MOD 1000000007
@@ -17,16 +19,65 @@ struct Node {
     int next;     // 右手拉的小动物
     int h;        // 欢乐值
     int f;        // 感动值
+    int t;        // 圈的大小
 } node[MAX_N + 5];
 
 int n, m;
+int vis[MAX_N + 5];
 
-void change(int p, int q) {
+void init() {
+    for (int i = 1; i <= n; i++) {
+        node[i].id = i;
+        node[i].t = n;
+        node[i].next = i + 1;
+        node[i].pre = i - 1;
+        i == 1 && (node[i].pre = n);
+        i == n && (node[i].next = 1);
+    }
+    return ;
+}
+
+// 更新圈的大小
+void update() {
 
 }
 
-void output() {
+void change(int p, int q) {
+    node[node[p].next].pre = node[q].pre;
+    node[node[q].pre].next = node[p].next;
+    node[p].next = q;
+    node[q].pre = p;
+    update();
+    return ;
+}
 
+// 以动物x为基准产生的能量
+LL getSum(int x) {
+    int st = node[x].next, cnt = 1;
+    LL sum = 0;
+    while (node[st].id != x) {
+        sum = (sum + (((node[x].t - cnt) % MOD) * (node[st].h % MOD) * (node[st].f % MOD)) % MOD) % MOD;
+        st = node[st].next;
+        cnt++;
+    }
+    return sum;
+}
+
+void func() {
+    LL sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = (sum + getSum(i)) % MOD;
+    }
+    cout << sum << endl;
+}
+
+
+void output() {
+    cout << "队形" << endl;
+    for (int i = 1; i <= n; i++) {
+        cout << node[i].pre << " " << node[i].id << " " << node[i].next << endl;
+    }
+    return ;
 }
 
 int main() {
@@ -34,19 +85,21 @@ int main() {
     for (int i = 1; i <= n; i++) {
         cin >> node[i].h >> node[i].f;
     }
+    init();
+    //output();
     cin >> m;
     for (int i = 0; i < m; i++) {
         int k, p, q;
         cin >> k >> p >> q;
         if (k == 1) {
             change(p, q);
-            output();
+            func();
         } else if (k == 2) {
             node[p].h = q;
-            output();
+            func();
         } else if (k == 3) {
             node[p].f = q;
-            output();
+            func();
         }
     }
     return 0;
